af85ef94100cb1fccb349a5b2c0df63d5a6eaaca
Natka312 <ermolovanata312@gmail.com>
2022-11-07 01:43:26 +0200

    task 2

---
+++ JS/task-02.js
@@ -0,0 +1,20 @@
+const ingredients = [
+  'Potatoes',
+  'Mushrooms',
+  'Garlic',
+  'Tomatos',
+  'Herbs',
+  'Condiments',
+];
+//console.log(ingredients)
+const elements = document.querySelector("ingredients");
+const list = document.querySelector("ul");
+const arr = [];
+ingredients.forEach(item => {
+  const ingredient = document.createElement("li");
+  ingredient.classList.add("item");
+  ingredient.textContent = item;
+  arr.push(ingredient);
+});
+console.log(arr)
+list.append(...arr);
